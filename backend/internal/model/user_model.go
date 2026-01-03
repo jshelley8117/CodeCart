191c4c597d7664602294d282b0eb5daaaffc09a0
William Villagran <wvillagran03@gmail.com>
2026-01-03 11:20:46 -0600

    refactor: reverted int to string to align with db

--- backend/internal/model/user_model.go
+++ backend/internal/model/user_model.go
@@ -11,13 +11,13 @@ type User struct {
 	UpdatedAt  time.Time `json:"updated_at"`
 	IsActive   bool      `json:"is_active"`
 	CustomerId int       `json:"customer_id"`
-	GCAuthId   int       `json:"gc_auth_id"`
+	GCAuthId   string    `json:"gc_auth_id"`
 }
 
 type CreateUserRequest struct {
 	Email      string `json:"email" validate:"required,email"`
 	CustomerId int    `json:"customer_id" validate:"required"`
-	GCAuthId   int    `json:"gc_auth_id" validate:"required"`
+	GCAuthId   string `json:"gc_auth_id" validate:"required"`
 }
 
 type UserResponse struct {
