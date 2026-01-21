8464f5def559ad076c62852f7ff5ea418170e76c
jshelley8117 <jshelley8117@gmail.com>
2026-01-20 22:26:11 -0600

    refactor: update logging and http message response to display user-digestable messages

--- backend/internal/handler/order_handler.go
+++ backend/internal/handler/order_handler.go
@@ -33,25 +33,25 @@ func (oh OrderHandler) HandleCreateOrder(w http.ResponseWriter, r *http.Request)
 
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		zLog.Warn("request body read failed", zap.Error(err))
-		http.Error(w, common.ERR_REQ_BODY_READ_FAIL, http.StatusBadRequest)
+		zLog.Warn(common.ERR_REQ_BODY_READ_FAIL, zap.Error(err))
+		http.Error(w, common.ERR_CLIENT_REQUEST_FAIL, http.StatusBadRequest)
 		return
 	}
 
 	if err := json.Unmarshal(body, &request); err != nil {
-		zLog.Warn("go unmarshaling failed", zap.Error(err))
-		http.Error(w, common.ERR_REQ_UNMARSH_FAIL, http.StatusBadRequest)
+		zLog.Warn(common.ERR_REQ_UNMARSH_FAIL, zap.Error(err))
+		http.Error(w, common.ERR_CLIENT_REQUEST_FAIL, http.StatusBadRequest)
 		return
 	}
 
 	if err := validate.Struct(request); err != nil {
-		zLog.Warn("struct validation failed", zap.Error(err))
-		http.Error(w, common.ERR_VALIDATION_FAIL, http.StatusBadRequest)
+		zLog.Warn(common.ERR_VALIDATION_FAIL, zap.Error(err))
+		http.Error(w, common.ERR_CLIENT_REQUEST_FAIL, http.StatusBadRequest)
 		return
 	}
 	if err := oh.OrderService.CreateOrder(r.Context(), request); err != nil {
 		zLog.Error("service invocation failed", zap.Error(err))
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, common.ERR_CLIENT_DB_PERSISTENCE_FAIL, http.StatusInternalServerError)
 		return
 	}
 
