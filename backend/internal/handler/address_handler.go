51d097b0c8f88638ced9e18aa17866883117d6c7
jshelley8117 <jshelley8117@gmail.com>
2026-02-24 21:46:55 -0600

    refactor: add handler funcs after botched rebase attempt

--- backend/internal/handler/address_handler.go
+++ backend/internal/handler/address_handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"strconv"
 
 	"github.com/jshelley8117/CodeCart/internal/common"
 	"github.com/jshelley8117/CodeCart/internal/model"
@@ -73,4 +74,120 @@ func (ah AddressHandler) HandleGetAllAddresses(w http.ResponseWriter, r *http.Re
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(addressesApiResponse)
+}
+
+func (ah AddressHandler) HandleGetAddressById(w http.ResponseWriter, r *http.Request) {
+	zLog := utils.FromContext(r.Context(), zap.NewNop())
+	zLog.Debug("entered HandleGetAddressById")
+
+	idPathVal := r.PathValue("id")
+	if idPathVal == "" {
+		zLog.Error("ID field in endpoint path parameter is missing")
+		http.Error(w, "ID is empty", http.StatusBadRequest)
+		return
+	}
+
+	id, err := strconv.Atoi(idPathVal)
+	if err != nil {
+		zLog.Error("failed to convert id value from string to integer")
+		http.Error(w, "server failed to process ID value", http.StatusInternalServerError)
+		return
+	}
+
+	address, err := ah.AddressService.GetAddressById(r.Context(), id)
+	if err != nil {
+		zLog.Error("service invocation failed", zap.Error(err))
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	addressApiResponse, err := json.Marshal(address)
+	if err != nil {
+		zLog.Error(common.ERR_REQ_MARSH_FAIL, zap.Error(err))
+		http.Error(w, common.ERR_CLIENT_DB_RETRIEVAL_FAIL, http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(addressApiResponse)
+
+}
+
+func (ah AddressHandler) HandleUpdateAddressById(w http.ResponseWriter, r *http.Request) {
+	zLog := utils.FromContext(r.Context(), zap.NewNop())
+	zLog.Debug("entered HandleUpdateAddressById")
+
+	idPathVal := r.PathValue("id")
+	if idPathVal == "" {
+		zLog.Error("ID field in endpoint path parameter is missing")
+		http.Error(w, "ID is empty", http.StatusBadRequest)
+		return
+	}
+
+	id, err := strconv.Atoi(idPathVal)
+	if err != nil {
+		zLog.Error("failed to convert id value from string to integer")
+		http.Error(w, "server failed to process ID value", http.StatusInternalServerError)
+		return
+	}
+
+	var request model.UpdateAddressRequest
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		zLog.Error("request body read failed", zap.Error(err))
+		http.Error(w, common.ERR_REQ_BODY_READ_FAIL, http.StatusBadRequest)
+		return
+	}
+
+	if err := json.Unmarshal(body, &request); err != nil {
+		zLog.Error("go unmarshaling failed", zap.Error(err))
+		http.Error(w, common.ERR_REQ_UNMARSH_FAIL, http.StatusBadRequest)
+		return
+	}
+
+	if err := validate.Struct(request); err != nil {
+		zLog.Error("struct validation failed", zap.Error(err))
+		http.Error(w, common.ERR_VALIDATION_FAIL, http.StatusBadRequest)
+		return
+	}
+
+	err = ah.AddressService.UpdateAddressById(r.Context(), request, id)
+	if err != nil {
+		zLog.Error("service invocation failed", zap.Error(err))
+		http.Error(w, common.ERR_CLIENT_DB_RETRIEVAL_FAIL, http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+}
+
+func (ah AddressHandler) HandleDeleteAddressById(w http.ResponseWriter, r *http.Request) {
+	zLog := utils.FromContext(r.Context(), zap.NewNop())
+	zLog.Debug("entered HandleDeleteAddressById")
+
+	idPathVal := r.PathValue("id")
+	if idPathVal == "" {
+		zLog.Error("ID field in endpoint path parameter is missing")
+		http.Error(w, "ID is empty", http.StatusBadRequest)
+		return
+	}
+
+	id, err := strconv.Atoi(idPathVal)
+	if err != nil {
+		zLog.Error("failed to convert id value from string to integer")
+		http.Error(w, "server failed to process ID value", http.StatusInternalServerError)
+		return
+	}
+
+	err = ah.AddressService.DeleteAddressById(r.Context(), id)
+	if err != nil {
+		zLog.Error("service invocation failed", zap.Error(err))
+		http.Error(w, common.ERR_CLIENT_DB_RETRIEVAL_FAIL, http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
